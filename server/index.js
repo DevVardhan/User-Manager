47a8835322d2610f3e1da53c9d1cfe6d8387625c
Dev Vardhan Singh Bhadauria <devvardhan456@gmail.com>
2024-10-21 18:28:20 +0530

    api setup

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import connectDB from './database/connect.js'
+import UserRoutes from './routes/UserRoute.js'
+import alluserRoutes from './routes/allUsersRoute.js'
+import cors from 'cors'
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/api/users' ,UserRoutes );
+app.use('/api/allUsers' ,alluserRoutes );
+
+app.get("/" , async(req , res )=>{
+    res.send("Connected to database")
+})
+
+const startServer = () =>{
+    try{
+        connectDB(process.env.MONGO_URL);
+        app.listen(5000 , ()=>{
+            console.log("Server Started listening at PORT:5000")
+        })
+    }catch(err){
+        console.log(err);
+    }
+}
+
+startServer();
