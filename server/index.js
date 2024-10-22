813e22d80265a46f5046483b11528ecb6c956042
Dev Vardhan Singh Bhadauria <devvardhan456@gmail.com>
2024-10-22 19:32:05 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin :"https://user-manager-swart.vercel.app",
+        origin :"https://user-manager-swart.vercel.app/",
         methods: ["POST" , "GET"],
         credentials: true ,
     }
